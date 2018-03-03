2c626f9c1e6189d16d5156781ec2ab0ebd33a9a0
KOIZUMI Satoru <koizumistr@minos.ocn.ne.jp>
2018-03-03 14:42:07 +0900

    beautify

--- myjvn.rb
+++ myjvn.rb
@@ -12,27 +12,21 @@ def search(keyword)
   Net::HTTP::Proxy(@proxy_addr, @proxy_port).start('jvndb.jvn.jp', 80) do | session |
     time = Time.new
     start_year = time.year-3
-    puts start_year
+    puts "From " + start_year.to_s
     response = session.get("/myjvn?method=getVulnOverviewList&rangeDatePublic=n&rangeDatePublished=n&rangeDateFirstPublished=n&keyword=#{keyword}&lang=ja&xsl=1&dateFirstPublishedStartY=#{start_year}")
     if response.code != '200'
       STDERR.puts "#{response.code} - #{response.message}"
       return
     end
 
-#    puts response.body
     xml = REXML::Document.new(response.body)
-#    num = xml.root.get_elements('/rdf:RDF/channel/items/rdf:Seq/rdf:li')
 
-#   puts num
-#    puts num.size
     xml.root.elements.each('/rdf:RDF/item') do |item|
-#      p item
-      print item.elements['sec:identifier'].text,"\t",item.elements['title'].text, " : ", item.elements['dcterms:modified'].text,"\n"
+      print item.elements['sec:identifier'].text,"\n\t",item.elements['title'].text, "\n\t", item.elements['dcterms:modified'].text,"\n"
       sec_cvss = item.elements['sec:cvss']
-      print sec_cvss.attributes['vector'],"\t", sec_cvss.attributes['score'], "\n"
+      print "\t",sec_cvss.attributes['vector'],"\t", sec_cvss.attributes['score'], "\n"
     end
     
-    exit 0
   end
 end
 
